79b642a2a630739aafbec74800b9d4e083d40f52
Shaikh-Aman-github <askooli956@gmail.com>
2022-11-08 12:48:30 +0530

    first commit

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(cors())
 app.use(express.urlencoded({extended: false}));
 app.use(morgan('dev'));
 
-//app.use(require('./routes/auth'));
 app.use('/api', require('./routes/api.route'));
 
 
